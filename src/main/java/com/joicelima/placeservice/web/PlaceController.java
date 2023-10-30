9ce2cfe970519e2ff13410567ebef73796806d1c
JoiceStefany <steffanyjoice006@gmail.com>
2023-10-30 16:26:03 -0300

    API

--- src/main/java/com/joicelima/placeservice/web/PlaceController.java
+++ src/main/java/com/joicelima/placeservice/web/PlaceController.java
@@ -4,6 +4,7 @@ package com.joicelima.placeservice.web;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,7 @@ import reactor.core.publisher.Mono;
 
 
 @RestController
-@RequestMapping("/place")
+@RequestMapping("/places")
 public class PlaceController {
 
 
@@ -27,7 +28,7 @@ public class PlaceController {
 
 
     @PostMapping
-    public ResponseEntity<Mono<Place>> create(Place place){
+    public ResponseEntity<Mono<Place>> create(@RequestBody Place place){
         var createdPlace = placeService.create(place);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdPlace);
     }
